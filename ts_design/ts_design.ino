de295973ebad2be641aafc48d8672ab2d1a38796
Joe Sprankle <joe@Joes-MBP.socal.rr.com>
2018-11-29 22:38:11 -0800

    should be functional to send selection servo board after waiting after 6th coin entered

--- ts_design/ts_design.ino
+++ ts_design/ts_design.ino
@@ -1,18 +1,8 @@
 /*
-   Small Program to Simulate a Numpad using a 2.4" TFT Touchscreen
-   Program does not act as an USB HID Device, for reference purposes only
-   Tested on Arduino UNO Only and 0x9341
-   By William Tavares
-
-   Note:
-   This version is coplete with styling and numbers,
-   if you want the smaller version get the "numpad-layout" program
-   from my Github https://github.com/williamtavares/Arduino-Uno-NumPad
-
-   Open the Serial Monitor to see the program running
-   Enjoy!
-
-   320 x 240
+  Joe's Arduino Vendine Machine.
+  Two scripts, this one will accept payment and allow a button
+  1-4 fot selction of a product
+** TODO: have coins be counted and dsiapyed in row 3
 */
 
 #include <Adafruit_GFX.h>
@@ -24,6 +14,8 @@
 #define YM 9   // can be a digital pin
 #define XP 8   // can be a digital pin
 
+// This was borowed and i yea to have time to go back in and understand.
+// My buttons are pretty fat so etyballing the pints was easy.
 // calibration mins and max for raw data when touching edges of screen
 // YOU CAN USE THIS SKETCH TO DETERMINE THE RAW X AND Y OF THE EDGES TO GET YOUR HIGHS AND LOWS FOR X AND Y
 #define TS_MINX 210
@@ -39,7 +31,7 @@
 // optional
 #define LCD_RESET A4
 
-//Color Definitons
+//Color Definitons, more than needed, but there to have
 #define BLACK     0x0000
 #define BLUE      0x001F
 #define GREY      0xCE79
@@ -54,6 +46,8 @@
 #define MINPRESSURE 100
 #define MAXPRESSURE 1000
 
+int coinSensVal = 0;
+
 
 
 // For better pressure precision, we need to know the resistance
@@ -65,20 +59,24 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364);
 //Size of key containers 70px
 #define BOXSIZE 100
 
-//2.4 = 240 x 320
-//Height 319 to fit on screen
+
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
-//Container variables for touch coordinates
-int X, Y, Z;
+
 
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //IDE, can be commented out
+  Serial1.begin(9600); //dont touch this else the servo board will be down
+
+  //configure pin 52 as an input and enable the internal pull-up resistor, see here for a good primer
+  //on pull up, https://www.baldengineer.com/arduino-internal-pull-up-resistor-tutorial.html
+  pinMode(52, INPUT_PULLUP);
 
   tft.reset();
+
   uint16_t identifier = tft.readID();
   tft.begin(identifier);
 
@@ -91,47 +89,90 @@ void setup() {
   createButtons();
 
   Serial.println(F("Press any button on the TFT screen: "));
-
-
-
-
-
 }
 
 void loop() {
+  // my coin dummy tool was too fast without this, revisit when buiding the appliance
+  delay(1700);
+  //read the pushbutton value into a variable
+  int sensorVal = digitalRead(52);
+  //print out the value of the pushbutton
+  Serial.println("senosr is:");
+  Serial.println(sensorVal);
+  Serial.println("coin is:");
+  Serial.println(coinSensVal);
+  Serial.println("  ");
+  Serial.println("  ");
+  Serial.println("  ");
+  Serial.println("  ");
 
 
+  if (sensorVal == LOW)
+  {
+    coinSensVal++;
+    if (coinSensVal > 5)
+    {
+      //this is it, send a servot to light up
+      for (int i = 0; i <= 30000000; i++) {
+        //dostuff then delay, you should still catch the input
+        TSPoint p = ts.getPoint();
 
-  TSPoint p = ts.getPoint();
+        // pressure of 0 means no pressing!
+        if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+          delay(500);
+          //  Serial.print("X = "); Serial.print(p.x);
+          // Serial.print("\tY = "); Serial.print(p.y);
+          //   Serial.print("\tPressure = "); Serial.println(p.z);
+          if (p.x > 370   ) {
+            if (p.y < 640 && p.y < 280) {
+              Serial.print("ONE");
+              char mystr[1] = "1"; //String data
+              Serial.write(mystr, 5); //Write the serial data
+              i = 0;
+              break;
+            }
 
+            if (p.y > 450 && p.y < 600) {
+              Serial.print("THREE");
+              char mystr[1] = "3`"; //String data
+              Serial.write(mystr, 5); //Write the serial data
+              i = 0;
+              break;
+            }
+          }
+          if (p.x < 625  ) {
+            if (p.y < 640 && p.y < 280)  {
+              Serial.print("TWO");
+              char mystr[1] = "2`"; //String data
+              Serial.write(mystr, 5); //Write the serial data
+              i = 0;
+              break;
+            }
 
+            if (p.y > 450 && p.y < 600) {
+              Serial.print("FOUR");
+              char mystr[1] = "4`"; //String data
+              Serial.write(mystr, 5); //Write the serial data
+              i = 0;
+              break;
+            }
+          }
+        }
 
-  // pressure of 0 means no pressing!
-  //everything happens in here
-  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
-    delay(500);
-   //  Serial.print("X = "); Serial.print(p.x);
-     //Serial.print("\tY = "); Serial.print(p.y);
-    //   Serial.print("\tPressure = "); Serial.println(p.z);
-    if (p.x > 370   ) {
-      if (p.y < 640 && p.y < 280) {
-        Serial.print("ONE");
       }
 
-      if (p.y > 450 && p.y < 600) {
-        Serial.print("THREE");
-      }
+      coinSensVal = 0;
+
     }
-    if (p.x < 625  ) {
-      if (p.y < 640 && p.y < 280)  {
-        Serial.print("TWO");
-      }
-
-      if (p.y > 450 && p.y < 600) {
-        Serial.print("FOUR");
-      }
-    }
+    // digitalWrite(13, LOW); joe save these for reference to light up servo
   }
+
+
+
+
+
+
+
 }
 
 

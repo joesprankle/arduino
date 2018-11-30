d341fd5c739a17eef1a30e678b95cf8078513740
Joe Sprankle <joe@Joes-MBP.socal.rr.com>
2018-11-29 21:58:37 -0800

    first release my working 4 grid with area for credit ledger

--- ts_design/ts_design.ino
+++ ts_design/ts_design.ino
@@ -51,7 +51,7 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-#define MINPRESSURE 1
+#define MINPRESSURE 100
 #define MAXPRESSURE 1000
 
 
@@ -73,38 +73,7 @@ Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 //Container variables for touch coordinates
 int X, Y, Z;
 
-//Screen height without hidden pixel
-double tHeight = tft.height() - 1;
 
-//Centering the mid square
-double center = (tft.width() / 2) - (BOXSIZE / 2);
-
-//Space between squares
-double padding = 10;
-
-//Position of squares to the left and right of center
-double fromCenter = BOXSIZE + padding;
-
-//Second row Y-Axis position
-double secondRow = BOXSIZE + padding;
-
-//Third row Y-Axis position
-double thirdRow = secondRow + BOXSIZE + padding;
-
-//Fourth row Y-Axis position
-double fourthRow = thirdRow + BOXSIZE + padding;
-
-//Y-Axis align for all squares
-double verticalAlign = (tHeight - ((BOXSIZE * 4) + (padding * 3))) / 2;
-
-//Left column starting x posision
-double leftColPositionX = center - fromCenter;
-
-//Mid column starting x posision
-double midColPositionX = center;
-
-//Right column starting x posision
-double rightColPositionX = center + fromCenter;
 
 void setup() {
   Serial.begin(9600);
@@ -116,12 +85,14 @@ void setup() {
   //Background color
   tft.fillScreen(LIGHTGREY);
 
+  tft.setRotation(2);
+
   // draw num pad
   createButtons();
-  insertNumbers();
+
   Serial.println(F("Press any button on the TFT screen: "));
 
-  tft.setRotation(0);
+
 
 
 
@@ -130,159 +101,43 @@ void setup() {
 void loop() {
 
 
-  Serial.println(tft.height());
-  Serial.println(tft.width());
-  delay(2000);
 
-  retrieveTouch();
-  int boxHeightRow1 = verticalAlign + BOXSIZE;
-  int boxHeightRow2 = secondRow + BOXSIZE;
-  int boxHeightRow3 = thirdRow + BOXSIZE;
-  int boxHeightRow4 = fourthRow + BOXSIZE;
+  TSPoint p = ts.getPoint();
 
-  if (Z > MINPRESSURE && Z < MAXPRESSURE) {
 
-    //redraw numpad to clear old number
-    tft.fillScreen(LIGHTGREY);
-    createButtons();
-    insertNumbers();
-    //default text setup for number display on tft
-    tft.setCursor(100, 120);
-    tft.setTextColor(RED);
-    tft.setTextSize(9);
 
-    //Check if element clicked is in left column
-    if (X > leftColPositionX && X < (leftColPositionX + BOXSIZE)) {
-      //Check if element clicked is in row 1
-      if (Y > verticalAlign) {
-        if (Y < boxHeightRow1) {
-          Serial.println("1");
-          tft.println("1");
-          delay(150);
-        }
-        //Check if element clicked is in row 2
-        else if (Y < boxHeightRow2) {
-          Serial.println("4");
-          tft.println("4");
-          delay(150);
-        }
-        //Check if element clicked is in row 3
-        else if (Y < boxHeightRow3) {
-          Serial.println("7");
-          tft.println("7");
-          delay(150);
-        }
-        //Check if element clicked is in row 4
-        else if (Y < boxHeightRow4) {
-          Serial.println("0");
-          tft.println("0");
-          delay(150);
-        }
+  // pressure of 0 means no pressing!
+  //everything happens in here
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+    delay(500);
+   //  Serial.print("X = "); Serial.print(p.x);
+     //Serial.print("\tY = "); Serial.print(p.y);
+    //   Serial.print("\tPressure = "); Serial.println(p.z);
+    if (p.x > 370   ) {
+      if (p.y < 640 && p.y < 280) {
+        Serial.print("ONE");
       }
-      //Check if element clicked is in mid column
-    } else if (X > midColPositionX && X < (midColPositionX + BOXSIZE)) {
-      //Check if element clicked is in row 1
-      if (Y > verticalAlign) {
-        if (Y < boxHeightRow1) {
-          Serial.println("2");
-          tft.println("2");
-          delay(150);
-        }
-        //Check if element clicked is in row 2
-        else if (Y < boxHeightRow2) {
-          Serial.println("5");
-          tft.println("5");
-          delay(150);
-        }
-        //Check if element clicked is in row 3
-        else if (Y < boxHeightRow3) {
-          Serial.println("8");
-          tft.println("8");
-          delay(150);
-        }
-        //Check if element clicked is in row 4
-        else if (Y < boxHeightRow4) {
-          Serial.println("0");
-          tft.println("0");
-          delay(150);
-        }
-      }
-      //Check if element clicked is in third column
-    } else if (X > rightColPositionX && X < (rightColPositionX + BOXSIZE)) {
-      if (Y > verticalAlign) {
-        //Check if element clicked is in row 1
-        if (Y < boxHeightRow1) {
-          Serial.println("3");
-          tft.println("3");
-          delay(150);
-        }
-        //Check if element clicked is in row 2
-        else if (Y < boxHeightRow2) {
-          Serial.println("6");
-          tft.println("6");
-          delay(150);
-        }
-        //Check if element clicked is in row 3
-        else if (Y < boxHeightRow3) {
-          Serial.println("9");
-          tft.println("9");
-          delay(150);
-        }
-        //Check if element clicked is in row 3
-        else if (Y < boxHeightRow4) {
-          Serial.println(".");
-          tft.println(".");
-          delay(150);
-        }
+
+      if (p.y > 450 && p.y < 600) {
+        Serial.print("THREE");
       }
     }
+    if (p.x < 625  ) {
+      if (p.y < 640 && p.y < 280)  {
+        Serial.print("TWO");
+      }
 
-    // good for debuggin, prints out the x,y cordinates of the press
-    // tft.setTextSize(3);
-    // tft.print("X = "); tft.println(X);
-    // tft.print("Y = "); tft.println(Y);
-
+      if (p.y > 450 && p.y < 600) {
+        Serial.print("FOUR");
+      }
+    }
   }
 }
 
-void retrieveTouch()
-{
-  digitalWrite(13, HIGH);
-  TSPoint p = ts.getPoint();
-  digitalWrite(13, LOW);
 
-  //If sharing pins, you'll need to fix the directions of the touchscreen pins
-  pinMode(XM, OUTPUT);
-  pinMode(YP, OUTPUT);
-
-
-
-
-  //Scale from 0->1023 to tft.width
-  //X = map(p.x, TS_MAXX, TS_MINX, 0, tft.width());
-  //  Y = map(p.y, TS_MAXY, TS_MINY, 0, tft.height());
-
-  // on my tft the numbers are reversed so this is used instead of the above
-  X = tft.width() - map(p.x, TS_MAXX, TS_MINX, 0, tft.width());
-  Y = map(p.y, TS_MAXY, TS_MINY, 0, tft.height());
-  Z = p.z;
-
-
-
-}
 
 void createButtons() {
-  //(initial x,initial y,width,height,color)
-  double secondRowVertialAlign = secondRow + verticalAlign;
-  double thirdRowVertialAlign = thirdRow + verticalAlign;
-  double fourthRowVertialAlign = fourthRow + verticalAlign;
-  /*
-     Parameters
-    xStart : int, the horizontal position where the line starts
-    yStart : int, the vertical position where the line starts
-    width : int, the width of the rectangle
-    height : int, the height of the rectangle
-  */
+
 
   tft.setTextSize(4);
   tft.setTextColor(BLACK);
@@ -293,76 +148,32 @@ void createButtons() {
   //First Row
   tft.drawRect(10, 10, BOXSIZE, BOXSIZE, BLACK);
   tft.fillRect(10, 10, BOXSIZE, BOXSIZE, GREY);
-  tft.setCursor(60, 60);
+  tft.setCursor(45, 40);
   tft.println("1");
 
   tft.drawRect(130, 10, BOXSIZE, BOXSIZE, BLACK);
   tft.fillRect(130, 10, BOXSIZE, BOXSIZE, GREY);
+  tft.setCursor(170, 40);
+  tft.println("2");
 
   //Second Row
   tft.drawRect(10, 120, BOXSIZE, BOXSIZE, BLACK);
   tft.fillRect(10, 120, BOXSIZE, BOXSIZE, GREY);
+  tft.setCursor(45, 150);
+  tft.println("3");
 
   tft.drawRect(130, 120, BOXSIZE, BOXSIZE, BLACK);
   tft.fillRect(130, 120, BOXSIZE, BOXSIZE, GREY);
+  tft.setCursor(170, 150);
+  tft.println("4");
 
   //Third Row
   tft.drawRect(10, 230, 220, 85, BLACK);
   tft.fillRect(10, 230, 220, 85, GREY);
+  tft.setCursor(40, 265);
+  tft.setTextSize(2);
+  tft.println("Credit: $1.50");
 
 
 
-
-}
-
-void insertNumbers() {
-  //Centers text horizontally on all three columns
-  double leftColCursorX   = leftColPositionX + (BOXSIZE / 2);
-  double midColCursorX    = midColPositionX  + (BOXSIZE / 3);
-  double rightColCursorX  = rightColPositionX + (BOXSIZE / 2);
-  //Centers text horizontally on all four rows
-  double firstRowCursorY  = verticalAlign + (BOXSIZE / 2);
-  double secondRowCursorY = secondRow + firstRowCursorY;
-  double thirdRowCursorY  = thirdRow  + firstRowCursorY;
-  double fourthRowCursorY = fourthRow + firstRowCursorY;
-
-  tft.setTextSize(4);
-  tft.setTextColor(BLACK);
-
-  //Insert Number 1
-
-
-
-
-  //Insert Number 3
-  tft.setCursor(rightColCursorX, firstRowCursorY);
-  tft.println("3");
-
-  //Insert Number 4
-  tft.setCursor(leftColCursorX, secondRowCursorY);
-  tft.println("4");
-
-
-
-  //Insert Number 6
-  tft.setCursor(rightColCursorX, secondRowCursorY);
-  tft.println("6");
-
-  //Insert Number 7
-  tft.setCursor(leftColCursorX, thirdRowCursorY);
-  tft.println("7");
-
-
-
-  //Insert Number 9
-  tft.setCursor(rightColCursorX, thirdRowCursorY);
-  tft.println("9");
-
-  //Insert Number 0
-  tft.setCursor(leftColPositionX + BOXSIZE, fourthRowCursorY);
-  tft.println("0");
-
-  //Insert Period Character
-  tft.setCursor(rightColCursorX, fourthRowCursorY);
-  tft.println(".");
 }
